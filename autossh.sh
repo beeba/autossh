b2508dd02fddf984a49ad0688815c94bbfdfc031
beeba <norman.choe@gmail.com>
2012-02-27 15:44:18 -0500

    If you put ~/.ssh/id_rsa.sshvpn it won't work; use the full path /home/username/.ssh/id_rsa.sshvpn aha.

--- autossh.sh
+++ autossh.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-TUNNELCONFIG="-R 9922:localhost:22 -R 9980:localhost:80 -N -i ~/.ssh/id_rsa.sshvpn -o ServerAliveInterval=60"
+TUNNELCONFIG="-R 9922:localhost:22 -R 9980:localhost:80 -N -i /home/username/.ssh/id_rsa.sshvpn -o ServerAliveInterval=60"
 REMOTEUSER="sshvpn"
 REMOTEHOST="remotehost.example.tld"
 
